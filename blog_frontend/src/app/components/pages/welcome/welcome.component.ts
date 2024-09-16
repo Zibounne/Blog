11f6a72a8fe90cdc9ec70231d2903f686d42065b
DAHHANE Tarik <tarikdahhane@hotmail.fr>
2024-09-16 16:12:41 +0200

    Adding : Welcome | UX improvements for stylesheet and component

--- blog_frontend/src/app/components/pages/welcome/welcome.component.ts
+++ blog_frontend/src/app/components/pages/welcome/welcome.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, AfterViewInit, Inject, PLATFORM_ID, OnDestroy } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { trigger, style, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-welcome',
@@ -15,24 +14,94 @@ import { trigger, style, transition, animate, query, stagger } from '@angular/an
   ]
 })
 
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
   /////////////////////// Property ////////////////////////
 
   private sections: HTMLElement[] = [];
-  private currentSectionIndex = 0;
-  private isScrolling = false;
+  private currentSectionIndex: number = 0;
+  private isScrolling: boolean = false;
+  private scrollTimeout: any;
 
   ////////////////////// Constructor //////////////////////
 
-  constructor(private titleService: Title) { }
+  constructor(
+    private titleService: Title,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
 
   //////////////////////// Methods ////////////////////////
+
+  /* =========== Cycles =========== */
 
   // Init
   ngOnInit(): void {
     this.titleService.setTitle("Blog | Welcome");
+    
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.add('no-scroll');
+    }
   }
 
+  // After Init | Capture all sections
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.sections = Array.from(document.querySelectorAll('section'));
+      this.scrollToSection(0);
+    }
+  }
 
+  // Destroy | Reactivates scrolling when leaving this page
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.remove('no-scroll');
+    }
+  }
+
+  /* =========== Listener =========== */
+
+  // HostListener | Method to handle scrolling
+  @HostListener('window:wheel', ['$event'])
+  onScroll(event: WheelEvent) {
+    if (!isPlatformBrowser(this.platformId) || this.isScrolling) return;
+
+    if (event.deltaY > 0) {
+      this.nextSection();
+    } else {
+      this.previousSection();
+    }
+  }
+
+  // Go to next section
+  private nextSection(): void {
+    if (this.currentSectionIndex < this.sections.length - 1) {
+      this.scrollToSection(this.currentSectionIndex + 1);
+    }
+  }
+
+  // Go to previous section
+  private previousSection(): void {
+    if (this.currentSectionIndex > 0) {
+      this.scrollToSection(this.currentSectionIndex - 1);
+    }
+  }
+
+  // Scroll to the section based on the index
+  private scrollToSection(index: number): void {
+    if (this.isScrolling) return;
+
+    this.isScrolling = true;
+    this.currentSectionIndex = index;
+    const section = this.sections[this.currentSectionIndex];
+    
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(() => {
+      this.isScrolling = false;
+    }, 500);
+  }
 }
